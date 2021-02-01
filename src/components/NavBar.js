057de36f6ed6ffeca6c8ebb33db0a40ad1e71b0f
Jang Belche <jang.belche@student.manchester.ac.uk>
2021-02-01 18:19:21 +0000

    language toggle

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,8 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
-
+import ReactCountryFlag from "react-country-flag";
+import Switch from "react-switch";
 import landingPic from '../pics/landingPic.jpg'
 
 import '../App.css';
@@ -13,17 +14,45 @@ class NavBar extends Component {
     constructor(props) {
         super(props);
         this.handleToggle = this.handleToggle.bind(this);
+        this.onChange = this.onChange.bind(this);
         this.state = {
             bgColor : 'blue',
             navSize : '50%',
             opacity: 1,
             displayImg: '',
             brandSize: 50,
-            fontSize: 30
+            fontSize: 30,
+            checked: true,
+            language: 'en',
+            loaded: false
         }
     }
 
     componentDidMount() {
+        //setting language
+        var lang = localStorage.getItem("lang");
+        if(lang){
+            this.setState({
+                language: lang
+            })
+        }else{
+            localStorage.setItem("lang", this.state.language);
+        }
+        
+        if(lang === 'en'){
+            this.setState({
+                checked:true // true is US
+            })
+        }else{
+            this.setState({
+                checked:false //false is VE
+            })
+        }
+        this.setState({
+            loaded: true
+        })
+
+        //navbar sizings
         if (typeof window !== "undefined") {
             window.onscroll = () => {
             let currentScrollPos = 500 - window.pageYOffset;
@@ -137,51 +166,113 @@ class NavBar extends Component {
                 navSize: '15%'
             });
         } 
+    }
+    onChange(){
+        this.setState({
+            checked: !this.state.checked
+        })
+        if(this.state.language=== 'en'){
+            localStorage.setItem("lang", 'sp');
+        }else{
+            localStorage.setItem("lang", 'en');
+        }
+        window.location.reload(false);
 
         
     }
+
+        
     render(){
-        return( 
-            <div>
-                <Navbar  inverse="true" collapseOnSelect fixed="top" className='nav-bar' bg={this.state.bgColor} variant="dark" expand="sm"
-                    onToggle={this.handleToggle}
-                 style={{height:this.state.navSize, position:'absolute'}}>
+        if(!this.state.loaded){
+            return(
+                <p>loading</p>
+            )
+        }else{
+            return( 
+                <div>
+                    <Navbar  inverse="true" collapseOnSelect fixed="top" className='nav-bar' bg={this.state.bgColor} variant="dark" expand="sm"
+                        onToggle={this.handleToggle}
+                    style={{height:this.state.navSize, position:'absolute'}}>
 
-                <Navbar.Brand style={{fontSize:this.state.brandSize+'px'}} href="/">Yakera</Navbar.Brand>
-                <Navbar.Toggle>
-                    <FontAwesomeIcon
-                        icon={faBars} color="white" size="2x" 
-                    />
-                </Navbar.Toggle>
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-                    <Nav.Link href="/info" style={{fontSize: this.state.fontSize + 'px'}}>Info</Nav.Link>
-                    <Nav.Link href="/donate" style={{fontSize: this.state.fontSize + 'px'}}>Donate</Nav.Link>
-                    <Nav.Link href="/campaigns" style={{fontSize: this.state.fontSize + 'px'}}>Campaigns</Nav.Link>
-                    <Nav.Link href="/faq" style={{fontSize: this.state.fontSize + 'px'}}>FAQ</Nav.Link>
-                    <Nav.Link href="/terms" style={{fontSize: this.state.fontSize + 'px'}}>Terms & Conditions</Nav.Link>
-                    {/* <Nav.Link href="/profile" style={{fontSize:'30px'}}>Profile</Nav.Link>
-                    <Nav.Link href="/login" style={{fontSize:'30px'}}>Log-in</Nav.Link> */}
-                    </Nav>                    
-
-                </Navbar.Collapse>
-                
-                </Navbar>
-                <div style={{textAlign:'center', maxHeight:'50%', minHeight:'50%', overflow: 'hidden'}}>
-                    <img  
-                    style={{
-                        minHeight:'100%',
-                        minWidht:'100%',
-                        display: this.state.displayImg,
-                        opacity: this.state.opacity
-                    }}
-                        width="100%"
-                        src={landingPic}
-                        alt="home-banner"
+                    <Navbar.Brand style={{fontSize:this.state.brandSize+'px'}} href="/">Yakera</Navbar.Brand>
+                    <Navbar.Toggle>
+                        <FontAwesomeIcon
+                            icon={faBars} color="white" size="2x" 
                         />
-                </div>                
-            </div>
-        )
+                    </Navbar.Toggle>
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mr-auto">
+                        <Nav.Link href="/info" style={{fontSize: this.state.fontSize + 'px'}}>Info</Nav.Link>
+                        <Nav.Link href="/donate" style={{fontSize: this.state.fontSize + 'px'}}>Donate</Nav.Link>
+                        <Nav.Link href="/campaigns" style={{fontSize: this.state.fontSize + 'px'}}>Campaigns</Nav.Link>
+                        <Nav.Link href="/faq" style={{fontSize: this.state.fontSize + 'px'}}>FAQ</Nav.Link>
+                        <Nav.Link href="/terms" style={{fontSize: this.state.fontSize + 'px'}}>Terms & Conditions</Nav.Link>
+                        {/* <Nav.Link href="/profile" style={{fontSize:'30px'}}>Profile</Nav.Link>
+                        <Nav.Link href="/login" style={{fontSize:'30px'}}>Log-in</Nav.Link> */}
+                        <div style={{marginLeft:'5px'}}>
+                            <Switch
+                            onChange={this.onChange}
+                            checked={this.state.checked}
+                            handleDiameter={25}
+                            offColor="#bebdbe"
+                            onColor="#bebdbe"
+                            offHandleColor="#01224d"
+                            onHandleColor="#01224d"
+                            height={50}
+                            width={100}
+                            borderRadius={10}
+                            uncheckedIcon={
+                                <div>
+                                <ReactCountryFlag
+                                        countryCode="VE"
+                                        svg
+                                        style={{
+                                            width: '50px',
+                                            height: '50px',
+                                            borderRadius:'15px',
+                                            paddingLeft:'5px'
+                                        }}
+                                        title="VE"
+                                    />
+                                </div>
+                            }
+                            checkedIcon={
+                                <ReactCountryFlag
+                                    countryCode="US"
+                                    svg
+                                    style={{
+                                        width: '50px',
+                                        height: '50px',
+                                        borderRadius:'15px',
+                                        paddingLeft:'5px'
+                                    }}
+                                    title="US"
+                                />
+                            }
+                            
+                            />
+                        </div>                        
+                        </Nav>                    
+
+                    </Navbar.Collapse>
+                    
+                    </Navbar>
+                    <div style={{textAlign:'center', maxHeight:'50%', minHeight:'50%', overflow: 'hidden'}}>
+                        <img  
+                        style={{
+                            minHeight:'100%',
+                            minWidht:'100%',
+                            display: this.state.displayImg,
+                            opacity: this.state.opacity
+                        }}
+                            width="100%"
+                            src={landingPic}
+                            alt="home-banner"
+                            />
+                    </div>                
+                </div>
+            )
+        }
     }
 }
 
--- src/components/Pages/Home/picsHome.js
+++ src/components/Pages/Home/picsHome.js
@@ -1,4 +1,4 @@
-export const pics = 
+export const en_pics = 
     {
         "education": require("../../../pics/education.png"),
         "business": require("../../../pics/business.png"),
@@ -13,4 +13,20 @@ export const pics =
         "banner3" : require('../../../pics/banner3.png'),
         "banner4" : require('../../../pics/banner4.png')
     }
+export const sp_pics = 
+    {
+        "education": require("../../../pics/education.png"),
+        "business": require("../../../pics/business.png"),
+        "nutrition": require("../../../pics/nutrition.png"),
+        "healthcare": require("../../../pics/healthcare.png"),
+        "logo" : require('../../../pics/logo.png'), 
+        "airTMlogo" : require('../../../pics/airtmlogo.png'),
+        "nutriendologo" : require('../../../pics/nutriendologo.jpg'),                      
+        "rotaracatlogo" : require('../../../pics/rotaracat.jpg'),
+        "banner1" : require('../../../pics/banner1_sp.png'),
+        "banner2" : require('../../../pics/banner2_sp.png'),
+        "banner3" : require('../../../pics/banner3_sp.png'),
+        "banner4" : require('../../../pics/banner4_sp.png')
+    }
+
 
