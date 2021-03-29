818c5c302df4eea03ff545e3154ded2bd1b3aec7
Jang Belche <jang.belche@student.manchester.ac.uk>
2021-03-29 12:44:33 +0100

    styling changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import NavBar from './components/NavBar';
 import Main from './components/Main';
+// import Footer from './components/footer';
 
 //import Construction from './components/Pages/Construction/construction';
 
@@ -19,6 +20,7 @@ function App() {
           <div>
             <NavBar />
             <Main />
+            {/* <Footer /> */}
           </div>
       </React.Fragment>
     
--- src/components/author.js
+++ src/components/author.js
@@ -14,7 +14,7 @@ class Author extends Component{
             return(
                 <div className='author-page'>
                         <hr style={{width:'80%', marginLeft:'10%' }}/>
-                        <div style={{textAlign:'center'}}>
+                        <div style={{textAlign:'center', padding:'20px'}}>
                             <p style={{
                                 color:'grey',
                                 fontSize:'20px'
@@ -52,7 +52,7 @@ class Author extends Component{
                 <div className='author-page'>
                         <hr style={{width:'80%', marginLeft:'10%' }}/>
 
-                        <div style={{textAlign:'center'}}>
+                        <div style={{textAlign:'center', padding:'20px'}}>
                             <p style={{
                                 color:'grey',
                                 fontSize:'20px'
