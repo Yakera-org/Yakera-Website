d123e2e199af5bd5049a9ee5cce7ca7d4cd31868
Jang <37816494+jango707@users.noreply.github.com>
2021-09-07 15:05:06 +0100

    magic

---
+++ src/components/Pages/AboutUs/AboutUs.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Author from '../../author'
+import './AboutUs.css'
+import AboutUsVisuals from './AboutUsVisuals'
+
+function AboutUs() {
+    return (
+        <div className='about-us-page'>
+            <AboutUsVisuals />
+            <Author />
+        </div>
+    )
+}
+
+export default AboutUs
