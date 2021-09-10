5b1b8ef0063ee31f4ced2f4e3366f630a7cab17c
Jang <jangbel99@gmail.com>
2021-09-10 13:10:22 +0100

    trigger discord

--- src/components/Pages/CreateCampaignPage/CreateCampaign.js
+++ src/components/Pages/CreateCampaignPage/CreateCampaign.js
@@ -120,7 +120,7 @@ function CreateCampaign() {
         var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
         return text.replace(urlRegex, function(url) {
             return '<a href="' + url + '">' + url + '</a>';
-        });
+        }); 
     }
 
     async function submit(event){
