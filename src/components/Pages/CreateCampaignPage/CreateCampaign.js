67ed6966d08056cbce4b99e5184688d5aeb84f19
Jang <jangbel99@gmail.com>
2021-09-13 23:20:54 +0100

    Merge Andres' code

--- src/components/Pages/CreateCampaignPage/CreateCampaign.js
+++ src/components/Pages/CreateCampaignPage/CreateCampaign.js
@@ -2,7 +2,6 @@ import React, {useState} from "react";
 import { validateCampaignFields } from "./Campaign_Validation";
 import CreateCampaignVisuals from "./CreateCampaignVisuals";
 import Author from '../../author';
-
 import * as axios from 'axios'
 
 
@@ -26,7 +25,8 @@ function CreateCampaign() {
     };
     const [data, setData] = useState(initialState);
     const [errorMessage, setError] = useState('');
-    const [images, setImages] = useState([]);
+    const [formattedStory, setStory] = useState('');
+    const [files, setFiles] = useState([]);
 
     const handleChange = event => {
 
@@ -34,10 +34,6 @@ function CreateCampaign() {
         let value = event.target.value;
 
         name = name.toLowerCase();
-
-        if(name === 'campaignpics' || name === 'documents' || name === 'mainpic'){
-            value = event.target.files
-        }
         
 
         setData({
@@ -57,26 +53,29 @@ function CreateCampaign() {
             })
         }
 
-
         return
     };
 
     const handleImageChange = event => {
-        if (event.target.files && event.target.files.length > 0) {
+        const name = event.target.name;
+        if (event.target.files && event.target.files.length > 1) {
             for (const file of event.target.files) {
-                const filename = file.name;
                 const reader = new FileReader();
                 reader.readAsDataURL(file);
                 reader.onload = () => {
-                    setImages(images => [...images, { 
-                        filename: filename,
-                        buffer: reader.result,
-                        file: file
-                    }]);
-                }
+                    setFiles(files => [...files, { name: name, uploadedFile: file }]);
+                };
             }
+            return;
         }
+        const reader = new FileReader();
+        const file = event.target.files[0];
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+            setFiles(files => [...files, { name: name, uploadedFile: file }]);
+        };
     };
+
     function validateData(){
         let emptyWarning = 'This field cannot be empty';
         let nameError, amountError, storyError, budgetError;
@@ -118,7 +117,7 @@ function CreateCampaign() {
         return false
       }
 
-    function linkify(text) {
+      function linkify(text) {
         //copied from https://stackoverflow.com/questions/1500260/detect-urls-in-text-with-javascript
         var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
         return text.replace(urlRegex, function(url) {
@@ -126,31 +125,64 @@ function CreateCampaign() {
         }); 
     }
 
+
     async function submit(event){
         event.preventDefault();
-
-        
         let formattedStory = linkify(data.story)
         formattedStory = formattedStory.replace(/\n/g, " <br />");
-        
-        console.log(formattedStory)
+        setStory(formattedStory)
 
         let isValidated = validateData()
+        console.log(data)
+
         if(isValidated){
             submitToBackend()
         }else{
             setError('Some info is not correct, please check the fields.')
         }
-       
     }
 
     async function submitToBackend(){
-        const res = await axios.post();
+        //https://dev.to/fadiamg/multiple-file-inputs-with-one-submit-button-with-react-hooks-kle
+        const formdata = new FormData();
+        for (const file of files) {
+            formdata.append(file.name, file.uploadedFile);
+        }
+        // const categories = {
+        //     'Small Business': 'small_business',
+        //     'Healthcare': 'healthcare',
+        //     'Education': 'education',
+        //     'Nutrition': 'nutrition'
+        // }
+        const payload = {
+            title: data.campaignname,
+            targetAmount: data.amount,
+            story: formattedStory,
+            category: data.campaigncategory,
+            description: 'Help Nutriendo El Futuro provide books and school supplies to dozens of children in El Calvario and buy chairs and tables for their community kitchen.'
+        }
+        for ( var key in payload ) {
+            formdata.append(key, payload[key]);
+        }
+        // Heroku Live
+        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMTM1OWExZGE4MTI0MDAwNDc5OGUyOSIsImlhdCI6MTYzMTQxMjI5Nn0.AaOAzUfKvuIyL2j6VSjI0DtCRb7W3nck7ohfxFCu3TE';
+        // Heroku Local
+        // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMjcyZTgxZGZkYTc4MTEzYzE5NTBlMCIsImlhdCI6MTYzMTM5NjEzMn0.4vs08muUTucSu9Pi5HUwA6oStxyVrcBYZlh-x0TzAGw'
+        const url = 'https://express-backend-api.herokuapp.com/api/campaigns';
+        const res = await axios.post(url, formdata, { 
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
         console.log(res.data);
+        
     }
+
+            
+       
     return (
         <div>
-            <CreateCampaignVisuals error={errorMessage} data={data} handleChange={handleChange} handleImageChange={handleImageChange} validate={validateData} submit={submit}/>
+             <CreateCampaignVisuals error={errorMessage} data={data} handleChange={handleChange} handleImageChange={handleImageChange} validate={validateData} submit={submit}/>
             <Author />
         </div>
     )
