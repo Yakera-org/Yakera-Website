6c2881d25d1f38522f31b5115b29975f68e38cb7
Jang <jangbel99@gmail.com>
2021-09-23 12:53:45 +0100

    New About us page

--- src/components/Pages/AboutUs/AboutUsVisuals.js
+++ src/components/Pages/AboutUs/AboutUsVisuals.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Grid } from '@material-ui/core';
-import titleImg from '../../../pics/y.png'
 import ContentSection from './ContentSection';
-import illustracion1 from '../../../pics/ill_1.PNG'
-import illustracion2 from '../../../pics/ill_2.PNG'
-import illustracion3 from '../../../pics/ill_3.PNG'
-import illustracion4 from '../../../pics/ill_4.PNG'
 import content from './texts.json'
 import TeamSection from './TeamSection';
 
+const illustracion1 = 'https://yakera-files.s3.us-east-2.amazonaws.com/yakera/illustration-aboutus-1.png'
+const illustracion2 = 'https://yakera-files.s3.us-east-2.amazonaws.com/yakera/illustration-aboutus-2.png'
+const illustracion3 = 'https://yakera-files.s3.us-east-2.amazonaws.com/yakera/illustration-aboutus-3.png'
+const illustracion4 = 'https://yakera-files.s3.us-east-2.amazonaws.com/yakera/illustration-aboutus-4.png'
 const illustracions = [illustracion4, illustracion3, illustracion2, illustracion1]
+const titleImg = 'https://yakera-files.s3.us-east-2.amazonaws.com/yakera/photos-aboutus.png'
+const worldImg = 'https://yakera-files.s3.us-east-2.amazonaws.com/yakera/worldmap.png'
 
 
 function AboutUsVisuals() {
@@ -18,32 +19,54 @@ function AboutUsVisuals() {
         <div className='about-us-visuals'>
             <Grid container spacing={0} alignItems="center">
                     <Grid item xs={12} sm={6} >
+                        <div className='about-us-top-img'>
+                            <img src={titleImg} alt='top-img' />
+                        </div>
+                    </Grid>
+                    <Grid item xs={12} sm={6} className='about-title-text' >
+                        <p>
+                            Yakera unlocks international solidarity through empathy and storytelling. We revolutionize humanitarian aid, one story at a time.
+                        </p>
+                    </Grid>
+                    <Grid item xs={12} sm={12} >
                         <div className='about-us-top-text'>
                             <h1>Check out the <b>Campaigns</b> in
                             </h1>
                             <h1 id='labels'>
                                 <a id='green' href='/campaigns'>Education</a>,&nbsp;
-                                <a id='yellow' href='/campaigns'> Nutrition</a>,&nbsp;
-                                <a id='red' href='/campaigns'> Healthcare</a>&nbsp;and&nbsp;
-                                <a id='blue' href='/campaigns'> Small Business</a>
+                                <a id='yellow' href='/campaigns'>Nutrition</a>,&nbsp;
+                                <a id='red' href='/campaigns'>Healthcare</a>&nbsp;and&nbsp;
+                                <a id='blue' href='/campaigns'>Small Business</a>
                             </h1>
                         </div>
-                    </Grid>
-                    <Grid item xs={12} sm={6} >
-                        <div className='about-us-top-img'>
-                            <img src={titleImg} alt='top-img' />
-                        </div>
-                    </Grid>
-
+                    </Grid>                
                 <Grid item xs={12} sm={12} >
                     <div className='about-us-banner'>
                         <p>Read the stories written by people on the platform</p>
                     </div>
                 </Grid>
+                
+                <Grid item xs={12} sm={6} className='about-world-text' >
+                    <section>
+                        <div id='percent'>
+                            100%
+                        </div>
+                        <p>
+                            <b>Of what you chip in goes to people directly:</b>
+                            &nbsp;no overhead costs, no fees. We are changing how humanitarian aid is delivered around the world.
+                        </p>
+                    </section>
+                </Grid>
+                <Grid item xs={12} sm={6} >
+                    <div className='about-us-world-img'>
+                        <img src={worldImg} alt='top-img' />
+                    </div>
+                </Grid>
+                <Grid container spacing={0} style={{ alignItems:'flex-start', textAlign:'left'}}>
                 {
                     content.content.map((element,i) => {
                         return(
-                            <Grid key={i} item xs={12} sm={6} >
+                            <Grid key={i} item xs={12} sm={3} >
                                 <div className='about-us-content-section'>
                                     <ContentSection
                                         title={element.title}
@@ -55,6 +78,7 @@ function AboutUsVisuals() {
                         )                        
                     })
                 }
+                </Grid>
 
             </Grid>
 
--- src/components/Pages/AboutUs/TeamSection.js
+++ src/components/Pages/AboutUs/TeamSection.js
@@ -2,20 +2,6 @@ import React from 'react'
 import { Grid } from '@material-ui/core';
 import './AboutUs.css'
 import text from './texts.json'
-import pic1 from '../../../pics/raul.jpeg'
-import pic2 from '../../../pics/aron.jpeg'
-import pic3 from '../../../pics/thomas.JPG'
-import pic4 from '../../../pics/jang.png'
-import pic5 from '../../../pics/mafer.jpg'
-
-const pics = [pic1, pic3, pic5, pic4, pic2]
-const names = ['Tomas Munoz', 'Raul Romero', 'Mafer Bencoma', 'Jang Belche', 'Aaron Lambert']
-const posts = ['CFO', 'Founder & CEO', 'CMO', 'CTO', 'COO']
-const hrefs = ['https://www.linkedin.com/in/tomas-munoz-reyes-7b2202156/',
-                'https://www.linkedin.com/in/raul-romero-martinez/',
-                'https://www.linkedin.com/in/maferbencomo/',
-                'https://www.linkedin.com/in/jang-belche/',
-                'https://www.linkedin.com/in/aaron-lambert-b8b4b9183/']
 
 function TeamSection() {
     return (
@@ -28,27 +14,7 @@ function TeamSection() {
             <div className='about-us-team-pics'>
                 <Grid container spacing={0}>
                     {
-                        pics.map((pic,i) => {
-                            let sm;
-                            if(i<3){
-                                sm = 4
-                            }else{
-                                sm = 6
-                            }
-                            return(
-                                <Grid key={i} item xs={12} sm={sm}>
-                                    <div className='outside-img'>
-                                        <img src={pic} alt='profile-pics' /> 
-                                        <h1 className='name'>{names[i]}&nbsp;
-                                            <a id="icon" href={hrefs[i]} rel="noopener noreferrer"  target="_blank">
-                                                <i className="fab fa-linkedin" ></i>
-                                            </a>
-                                        </h1>
-                                        <h2 className='post'>{posts[i]}</h2>
-                                    </div>
-                                </Grid>
-                            )
-                        })
+                        
                     }
                     
                 </Grid>
