4e980775bc0cb8d87df1d534e0a2d87317307c61
Carlos Zerpa <zerpa2007@hotmail.com>
2021-10-29 15:16:39 -0400

    Updated share-card to the new design
    new library added (react-share)

--- src/components/Pages/CampaignPage/ShareCard.js
+++ src/components/Pages/CampaignPage/ShareCard.js
@@ -1,10 +1,11 @@
 import React, { Component, Fragment } from 'react';
-import {Dialog, Grid} from '@material-ui/core';
+import { Dialog, Grid } from '@material-ui/core';
 import classnames from 'classnames';
+import { FacebookShareButton, FacebookIcon, WhatsappIcon, WhatsappShareButton, TwitterIcon, TwitterShareButton, EmailIcon, EmailShareButton } from 'react-share'
 
 import './sharecard.css';
 
-class ShareCard extends Component{
+class ShareCard extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -15,79 +16,97 @@ class ShareCard extends Component{
         this.hanldeCopy = this.hanldeCopy.bind(this);
     }
 
-    hanldeCopy(){
+    hanldeCopy() {
         navigator.clipboard.writeText(window.location.href);
         this.setState({
             copied: true
         })
     }
-    componentDidMount(){
+    componentDidMount() {
         this.setState({
             loaded: true,
             copied: false
         })
     }
-    render(){ 
+    render() {
         let copied;
         const EN = this.state.EN
-       
-        if(this.state.copied){
+
+        if (this.state.copied) {
             copied = <p className="copied">{EN ? 'Copied!' : 'Copia!'}</p>
         }
-        if(!this.state.loaded){
-            return(
+        if (!this.state.loaded) {
+            return (
                 <div>
                     Loading
                 </div>
             )
-        }else{
+        } else {
 
-            return(
+            return (
                 <Fragment >
                     <Dialog
-                        fullWidth={true} 
-                        maxWidth='lg'                                 
+                        fullWidth={true}
+                        maxWidth='lg'
                         open={this.props.open}
                         onClose={this.props.onClose}
-                        className="share-dialog" 
-                                                                                                                                                 
-                    >
-                        
-                    <h1 >{EN ? 'Help by sharing' : 'Ayuda compartiendo'} </h1>
-                    <p id="share-help">
-                         {EN ? 'Fundraisers shared on social networks raise up to 5 times more!' : '¡Las recaudaciones de fondos compartidas en las redes sociales recaudan hasta 5 veces más!'}
-                    </p>
+                        className="share-dialog"
 
-                    <hr style={{width:'100%'}}/>
-                    
-                    <Grid container spacing={0}>
-                        <Grid item xs={8} sm={8}>
-                            <input
-                                type="text"
-                                name="share-url"
-                                readOnly={true}
-                                value={window.location.href}
-                                className={classnames(
-                                    'form-control'
-                                )} 
-                                style={{marginLeft:'15%', marginBottom:'15px', width:'90%'}}  
-                                onClick={this.hanldeCopy}                                  
-                            />
-                        </Grid>
-                        <Grid item xs={4} sm={4}>
-                            <button
-                                type="submit"
-                                className="btn btn-secondary btn-block copy-btn"    
-                                style={{marginLeft:'25%', width:'50%'}} 
-                                onClick={this.hanldeCopy}                   
+                    >
+
+                        <h1 >{EN ? 'Multiply your impact' : 'Multiplica tu impacto'}</h1>
+                        <p id="share-help">
+                            {EN ? 'Tell your friends & family to pitch in!' : '¡Comentale a tu familia y amigos que te den una mano!'}
+                        </p>
+
+                        <Grid container spacing={0}>
+                            <Grid item xs={12} sm={12} className='social-media'>
+                                <FacebookShareButton url={window.location.href} quote={EN ? 'Please help me spread the word and chip in!' : 'Por favor ayudame a compartir la camapaña y contribuye!'} className='social-button'>
+                                    <FacebookIcon size={50} logoFillColor='white' round={true} />
+                                </FacebookShareButton>
+                                <WhatsappShareButton url={window.location.href} className='social-button'>
+                                    <WhatsappIcon size={50} logoFillColor='white' round={true} />
+                                </WhatsappShareButton>
+                                <TwitterShareButton url={window.location.href} className='social-button'>
+                                    <TwitterIcon size={50} logoFillColor='white' round={true} />
+                                </TwitterShareButton>
+                                <EmailShareButton url={window.location.href} className='social-button'>
+                                    <EmailIcon size={50} logoFillColor='white' round={true} />
+                                </EmailShareButton>
+                            </Grid>
+                            <Grid item xs={6} sm={6}>
+                                <input
+                                    type="text"
+                                    name="share-url"
+                                    readOnly={true}
+                                    value={window.location.href}
+                                    className={classnames(
+                                        'form-control'
+                                    )}
+                                    style={{ marginLeft: '55%', marginBottom: '15px', width: '60%' }}
+                                    onClick={this.hanldeCopy}
+                                />
+                            </Grid>
+                            <Grid item xs={3} sm={3}>
+                                <button
+                                    type="submit"
+                                    className="btn btn-secondary btn-block copy-btn"
+                                    style={{ marginLeft: '35%', width: '50%' }}
+                                    onClick={this.hanldeCopy}
                                 >
-                                {EN ? 'Copy!' : 'Copia!'}
-                            </button>
+                                    {EN ? 'Copy!' : 'Copia!'}
+                                </button>
+                            </Grid>
+                            {copied}
+                            <Grid item xs={12}>
+                                <div>
+                                    <img src='https://assets.yakera.org/yakera/illustration-email.png' alt='gratitudImage' className='share-img' />
+                                    <img src='https://assets.yakera.org/yakera/pattern-email.png' alt='gratitud' className='share-img-2' />
+                                </div>
+                            </Grid>
                         </Grid>
-                    </Grid>  
-                    {copied}            
                     </Dialog>
-                    </Fragment>                   
+                </Fragment>
             )
         }
     }
