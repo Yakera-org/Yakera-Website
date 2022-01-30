e99b1833b3742cf9ee592f2509c817c65c6e22f5
Albert Yang <65566664+Albertyangyhm@users.noreply.github.com>
2022-01-30 14:00:16 -0500

    Fixed Text Centering In Thanks Card
    create account button has not been done yet

--- src/components/Pages/CampaignPage/thanksCard.js
+++ src/components/Pages/CampaignPage/thanksCard.js
@@ -2,8 +2,29 @@ import React, { Component, Fragment } from 'react';
 import {Dialog, Grid, Hidden} from '@material-ui/core';
 
 class ThanksCard extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            loaded: false,
+        }
+        // console.log(this.state.EN);
+    }
+    componentDidMount() {
+        this.setState({
+            loaded: true
+        })
+    }
+
 
     render(){
+
+        if (!this.state.loaded) {
+            return (
+                <div>
+                    Loading
+                </div>
+            )
+        } else {
         return(
             <Fragment >
                 <Dialog
@@ -14,66 +35,86 @@ class ThanksCard extends Component{
                     className="thanks-dialog"
 
                 >
-                <Grid container spacing={0} bgcolor= "#fbefe2" style = {{backgroundColor : "#fbefe2", padding: "5px 60px"}}>
+                <div style = {{backgroundColor : "#fbefe2"}}>
+                <div className='close-icon' onClick={this.props.onClose} style = {{float: "right"}}>
+                    <i className="fas fa-2x fa-times"></i>
+                </div>
+                </div>
 
-                    <Grid container
-                        spacing={0}
-                        direction="column"
-                        alignItems="center"
-                        justifyContent="center"
-                        style={{ minHeight: '30vh' }}
-                        sm = {6}
-                    >
+                <Grid container
+                 spacing={0}
+                 bgcolor= "#fbefe2"
+                 style = {{backgroundColor : "#fbefe2", padding: "0px 60px 30px 60px"}}
+                >
 
-                        <div>
+                      <Grid container
+                          spacing={0}
+                          direction="column"
+                          alignItems="center"
+                          justifyContent="center"
+                          style={{ minHeight: '30vh' }}
+                          sm = {6}
+                          item={true}
+                      >
 
-                        <h1 style={{marginTop:'10px', color:'#ea8737'}}> {true ? <b>Thank you!</b> : <b>Gracias!</b>}</h1>
-                        <p style = {{color : "#ea8737"}}>
+                          <div>
+
+                          <h1 style={{marginTop:'10px', color:'#ea8737'}}> {this.props.EN ? <b>Thank you!</b> : <b>Gracias!</b>}</h1>
+                          <br/>
+                          <p style = {{color : "#ea8737"}}>
+                              {this.props.EN ?
+                                <a>Yakera means gratitude in Warao,<br/> and we send you gratitude!</a>
+                                    :
+                                <a>Yakera significa gratitud en warao,<br/> y te mandamos nuestra Yakera</a>
+                              }
+                          </p>
+
+                          <hr width = "50%" color = "#ea8737"/>
+
+                          <p>
+
+                              {this.props.EN ? 'Thank you for your donation of ' : 'Gracias por tu donación de '}
+
+                                  <b>
+                                      ${this.props.amount}
+                                  </b>
+                                  {this.props.EN ? ' to the campaign: ' : ' A la campaña: '} <br/>
+
+                                  <b  style={{fontSize:'20px'}}>{this.props.title} </b>
+                          </p>
+
+                          <a
+                              href="https://form.jotform.com/212647238863160"
+                              title="Feedback"
+
+                          ><br/>
+                              {this.props.EN ? <b style={{ color: "#FF9800" }}>Click here to leave us feedback! </b>
+                              :
+                                <b style={{ color: "#FF9800" }}>Haz click aquí para dejarnos tu feedback! </b>
+                              }
+
+                          </a><br/><br/>
+
+                          <button onClick={this.props.onClose} style={{
+                              width:'50%',
+                              margin:'10px',
+                              // marginLeft:'25%',
+                              border:'none',
+                              backgroundColor:'#ea8737',
+                              borderRadius:'30px',
+                              color:'white',
+                              padding:'10px',
+                              fontSize: '13px'
+                          }}>
                             {true ?
-                              <div>Yakera means gratitude in Warao,<br/> and we send you gratitude!</div>
+                              'Create an Account'
                                   :
-                              <div>Yakera significa gratitud en warao,<br/> y te mandamos nuestra Yakera</div>
+                              'Crear una Cuenta'
                             }
-                        </p>
+                          </button>
+                          </div>
 
-                        <hr width = "50%" color = "#ea8737"/>
-
-                        <p>
-
-                            {true ? 'Thank you for your donation of ' : 'Gracias por tu donación de'}
-
-                                <b>
-                                    ${this.props.amount}
-                                </b>
-                                {true ? ' to the campaign: ' : 'A la campaña: '} <br/>
-
-                                <b>{this.props.title} </b>
-                        </p>
-
-                        <a
-                            href="https://form.jotform.com/212647238863160"
-                            title="Feedback"
-
-                        >
-                            <b style={{ color: "#FF9800" }}>Click here </b>to leave us feedback!
-                        </a>
-
-                        <button onClick={this.props.onClose} style={{
-                            width:'50%',
-                            margin:'10px',
-                            // marginLeft:'25%',
-                            border:'none',
-                            backgroundColor:'#ea8737',
-                            borderRadius:'30px',
-                            color:'white',
-                            padding:'10px',
-                            fontSize: '13px'
-                        }}>
-                            Return to Campaign
-                        </button>
-                        </div>
-
-                    </Grid>
+                      </Grid>
                     <Hidden xsDown>
                     <Grid container
                         spacing={0}
@@ -82,8 +123,7 @@ class ThanksCard extends Component{
                         justifyContent="center"
                         style={{ minHeight: '50vh' }}
                         sm = {6}
-                        only = "sm"
-
+                        item={true}
                     >
                         <img src='https://assets.yakera.org/yakera/illustration-share.webp' alt='gratitudImage' width = "90%"/>
 
@@ -94,7 +134,7 @@ class ThanksCard extends Component{
                 </Dialog>
             </Fragment>
         )
-    }
+    }}
 }
 
 export default ThanksCard;
