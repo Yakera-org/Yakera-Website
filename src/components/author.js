82235d0b3d0b45079c03ab2338eda634eed69e6e
TRINIDAD MUÑOZ <trinimreyes@uc.cl>
2022-04-19 12:08:58 -0400

    add datacamp logo

--- src/components/author.js
+++ src/components/author.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 import logo from "../svg/logo.svg";
+import datacamplogo from "../pics/datacamplogo.png";
 import LanguageService from "../services/language";
 
 import './author.css';
@@ -94,6 +95,9 @@ function Author() {
             <hr />
             <div className='section'>
                 {EN ? <p>This page is developed by the <b>Software Engineering Team</b> at <b>Yakera</b> and belongs to the Yakera company</p> : <p>Esta página está desarrollada por el <b> Equipo de Ingeniería de Software </b> en <b> Yakera </b> y pertenece a la empresa Yakera</p>} 
+            </div>
+            <div className='section'>
+                {EN ? <p> Partnering with  <img class='footer-img' alt='datacamp-logo' src={datacamplogo}/>  for data education </p> : <p> Asociación con  <img class='footer-img' alt='datacamp-logo' src={datacamplogo}/>  para la educación de datos </p>}
             </div>  
         </div>
     )
