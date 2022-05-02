08f706d3b22274a9ec3466048b448bb74dc29b6f
BravoAndres <68721629+BravoAndres@users.noreply.github.com>
2022-05-02 19:06:26 -0400

    Prepare Reserve donation payload to send to backend

--- src/components/Pages/CampaignPage/ReserveLogic.js
+++ src/components/Pages/CampaignPage/ReserveLogic.js
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+
+import api from "../../../services/api";
+import LanguageService from "../../../services/language";
+
 import ReserveVisual from './ReserveVisual';
 
 
@@ -16,6 +20,27 @@ function ReserveLogic(props) {
             [event.target.name]: event.target.value
         });
     };
+
+    async function sendToBackend() {
+        try {
+          const payload = {
+            slug: props.slug,
+            email: props.email,
+            name: props.name,
+            amount: props.amount,
+            tip: props.tip,
+            status: "pending",
+            comment: props.comment,
+            isAnonymous: props.isAnon,
+            language: LanguageService.getLanguage(),
+            paymentMethod: "reserve",
+            reserveUsername: data.username,
+          };
+          await api.post(`/campaigns/donate`, payload);
+        } catch (err) {
+          console.log(err);
+        }
+    }    
     
     return (
         <div>
