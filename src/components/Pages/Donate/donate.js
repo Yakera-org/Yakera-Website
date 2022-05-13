377c634edfda3fbaa3d1b050d9bc184c4cded1c1
remakh <reean.m@gmail.com>
2022-05-13 21:48:00 +0100

    Removes unnecessary function

--- src/components/Pages/Donate/donate.js
+++ src/components/Pages/Donate/donate.js
@@ -12,19 +12,6 @@ import api from '../../../services/api';
 import LanguageService from '../../../services/language';
 
 
-// no way did i write this, so here is an explanation
-// https://stackoverflow.com/questions/1584370/how-to-merge-two-arrays-in-javascript-and-de-duplicate-items
-const arrayUnique = (array) => {
-    var a = array.concat();
-    for (var i=0; i<a.length; i++) {
-        for (var j=i+1; j<a.length; ++j) {
-            if (a[i] === a[j])
-                a.splice(j--, 1);
-        }
-    }
-    return a;
-}
-
 const filterCampaignsBySearch = (campaigns, query, lang) => {
     if(!query) {
       return campaigns;
@@ -41,7 +28,7 @@ const filterCampaignsBySearch = (campaigns, query, lang) => {
         return campaignDescription.includes(query.toLowerCase());
     });
 
-    return arrayUnique(searchTitles.concat(searchDescriptions));
+    return Set(searchTitles.concat(searchDescriptions));
 };
 
 const filterCampaignsByCategory = (campaigns, categoryState) => {
