683ee205ad7b98c231a3880fcde6df1974c72f84
Jang <jangbel99@gmail.com>
2022-05-24 11:58:26 +0100

    Cconnect pageination to campaighns.

--- src/components/Pages/Campaigns/Pageination.js
+++ src/components/Pages/Campaigns/Pageination.js
@@ -2,7 +2,7 @@ import React from 'react';
 import HashLoader from "react-spinners/HashLoader";
 import { Grid } from '@material-ui/core';
 import CampaignCard from './campaignCard';
-import { ArrowUpward, ArrowDownward, ArrowForward, ArrowBack } from '@material-ui/icons';
+import { ArrowForward, ArrowBack } from '@material-ui/icons';
 import ReactPaginate from 'react-paginate';
 
 function Pageination(props) {
@@ -34,7 +34,7 @@ function Pageination(props) {
                         loading={true}
                         />
                 </div>
-                <section style={{height:"200px"}}>
+                <section style={{height:"500px"}}>
                     
                 </section>
             </div>
@@ -63,10 +63,10 @@ function Pageination(props) {
                     activeClassName="active"
                     forcePage={props.page-1}
                 />
-                 <Grid container spacing={0} style={{alignContent:'center', alignItems:'flex-start'}}>
+                 <Grid className="cam-container" container spacing={0} style={{alignContent:'center', alignItems:'flex-start'}}>
                         {props.campaigns.map((cam, i) => {
                             return (
-                                <Grid item xs={12} sm={3} key={i}>
+                                <Grid className='cam-card' item xs={12} sm={3} key={i}>
                                     <CampaignCard
                                         campaign={cam}
                                         language={EN ? "en" : "es"}
@@ -76,6 +76,28 @@ function Pageination(props) {
                                 )
                         })}
                     </Grid>
+
+                    <ReactPaginate
+                    breakLabel='...'
+                    nextLabel={<ArrowForward  />}
+                    previousLabel={<ArrowBack  />}
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={2}
+                    pageCount={props.pageCount}
+                    renderOnZeroPageCount={null}
+                    pageClassName="page-item"
+                    pageLinkClassName="page-link"
+                    previousClassName="page-item"
+                    previousLinkClassName="page-link"
+                    nextClassName="page-item"
+                    nextLinkClassName="page-link"
+                    breakClassName="page-item"
+                    breakLinkClassName="page-link"
+                    marginPagesDisplayed={1}
+                    containerClassName="pagination"
+                    activeClassName="active"
+                    forcePage={props.page-1}
+                />
             </div>
         );
     }
