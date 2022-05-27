0c396d3cca2456e5d3849b6122ab8c71e9032103
Jang <jangbel99@gmail.com>
2022-05-27 11:12:02 +0100

    Add Search button.

--- src/components/Pages/Campaigns/Searchbar.js
+++ src/components/Pages/Campaigns/Searchbar.js
@@ -1,37 +1,31 @@
+import React, {useState} from "react";
 import { Form, InputGroup } from 'react-bootstrap';
-import React from 'react';
 import SearchIcon from '@material-ui/icons/Search';
 
-class SearchBar extends React.Component {
-    render() {
-        const EN = this.props.EN;
-        return (
-            <InputGroup
-                className='searchbar'
-            >
-                <Form.Control
-                    type='search'
-                    placeholder={EN ? 'Search...' : 'Buscar...'}
-                    value={this.props.searchQuery}
-                    onChange={e => this.props.setSearchQuery(e.target.value)}
-                    style={{
-                        border: 'none',
-                        backgroundColor: '#f0f0f0',
-                    }}
-                />
-                <InputGroup.Text
-                    style={{
-                        border: 'none',
-                        backgroundColor: '#f0f0f0',
-                        borderRadius: 'initial',
-                        color: '#0f335f',
-                    }}
-                >
-                    <SearchIcon />
-                </InputGroup.Text>
-            </InputGroup>
-        )
-    }
-};
+function SearchBar(props) {
+
+    const [searchValue, setSearchValue] = useState("");
+
+    const EN = props.EN;
+    return (
+        <InputGroup
+            className='searchbar'
+        >
+            <Form.Control
+                type='search'
+                placeholder={EN ? 'Search...' : 'Buscar...'}
+                value={searchValue}
+                onChange={e => setSearchValue(e.target.value)}
+                style={{
+                    border: 'none',
+                    backgroundColor: '#f0f0f0',
+                }}
+            />
+            <InputGroup.Text >
+                <SearchIcon />
+            </InputGroup.Text>
+        </InputGroup>
+    )
+}
 
 export default SearchBar;
