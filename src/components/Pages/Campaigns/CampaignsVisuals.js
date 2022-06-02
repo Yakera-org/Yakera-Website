c246995f7e9f37c466fb0c69952922c1797d3860
Jang <jangbel99@gmail.com>
2022-06-02 11:00:16 +0100

    Show example Hihlight.

--- src/components/Pages/Campaigns/CampaignsVisuals.js
+++ src/components/Pages/Campaigns/CampaignsVisuals.js
@@ -4,6 +4,7 @@ import { Grid, Hidden } from '@material-ui/core';
 import pics from './pics';
 import SearchBar from "./Searchbar";
 import Pageination from "./Pageination";
+import HighlightedCampaign from "./HighlightedCampaign";
 
 const nameDictEN = {
     "education": "Education",
@@ -30,27 +31,11 @@ function CampaignsVisuals(props) {
                         <img alt='line-left' src={pics['line-left']} />
                     </Grid>
                 </Hidden>
-                <Grid item xs={6} sm={3} className='header-mid-img'>
-                    <img alt='donate-figure' src={pics['header-photo']} />
-                </Grid>
 
-                <Grid item xs={6} sm={3} className='mid-content'>
-                    <div className='main-text'>
-                        <b>{EN ? 'Support a story, ' : 'Apoya una historia, '}</b>
-                        <span>{EN ? 'change a life' : 'cambia una vida'}</span>
-                    </div>
-                                
-                    <div className='bubble-text'>
-                        <div className='bubble-quote'>
-                            <img alt='donate-figure' src="https://assets.yakera.org/yakera/profile-icon-1.webp" />
-                            <b> {"Sara López"} </b> <br />
-                            {EN ? 'For a world full of Yakera!' : '¡Por un mundo lleno de Yakera!'}
-                        </div>
-                        <div className='bubble-digit'>
-                            {'$20'}
-                        </div>
-                    </div>
+                <Grid item xs={12} sm={6}>
+                    <HighlightedCampaign EN={EN} campaign={props.highlightStory}/>
                 </Grid>
+                
                 <Hidden xsDown>
                     <Grid item={true} sm={3} className='header-right' style={{ textAlign: "center" }}>
                         <img
