75f1382836ec7662ca48d548252da56338283df3
Jang <jangbel99@gmail.com>
2022-06-02 11:45:30 +0100

    Remove pageination bar from loading screen.

--- src/components/Pages/Campaigns/Pageination.js
+++ src/components/Pages/Campaigns/Pageination.js
@@ -11,9 +11,30 @@ function Pageination(props) {
     function handlePageClick(e){
         props.setPage(e.selected)
     }
-
-    if(props.loading){
-        return(
+    return(
+        <div>
+            <ReactPaginate
+                breakLabel='...'
+                nextLabel={<ArrowForward  />}
+                previousLabel={<ArrowBack  />}
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={2}
+                pageCount={props.pageCount}
+                renderOnZeroPageCount={null}
+                pageClassName="page-item"
+                pageLinkClassName="page-link"
+                previousClassName="page-item"
+                previousLinkClassName="page-link"
+                nextClassName="page-item"
+                nextLinkClassName="page-link"
+                breakClassName="page-item"
+                breakLinkClassName="page-link"
+                marginPagesDisplayed={1}
+                containerClassName="pagination"
+                activeClassName="active"
+                forcePage={props.page-1}
+            />
+            {props.loading ?
             <div>
                 <div className='cam-loader'>
                     <HashLoader
@@ -26,75 +47,53 @@ function Pageination(props) {
                     
                 </section>
             </div>
-        )
-    }else{
-        return (
+                :
             <div>
-                <ReactPaginate
-                    breakLabel='...'
-                    nextLabel={<ArrowForward  />}
-                    previousLabel={<ArrowBack  />}
-                    onPageChange={handlePageClick}
-                    pageRangeDisplayed={2}
-                    pageCount={props.pageCount}
-                    renderOnZeroPageCount={null}
-                    pageClassName="page-item"
-                    pageLinkClassName="page-link"
-                    previousClassName="page-item"
-                    previousLinkClassName="page-link"
-                    nextClassName="page-item"
-                    nextLinkClassName="page-link"
-                    breakClassName="page-item"
-                    breakLinkClassName="page-link"
-                    marginPagesDisplayed={1}
-                    containerClassName="pagination"
-                    activeClassName="active"
-                    forcePage={props.page-1}
-                />
-                 <Grid className="cam-container" container spacing={0} style={{alignContent:'center', alignItems:'flex-start'}}>
-                        {props.campaigns.length > 0 ?                        
-                            props.campaigns.map((cam, i) => {
-                                return (
-                                    <Grid className='cam-card' item xs={12} sm={3} key={i}>
-                                        <CampaignCard
-                                            campaign={cam}
-                                            language={EN ? "en" : "es"}
-                                            amount={cam.raised + cam?.zelleRaised}
-                                        />
-                                    </Grid>
-                                    )
-                            })
-                            :
-                            <Grid className='no-cards' item xs={12} sm={12}>
-                                <p>{EN ? "No campaigns found that match the filters." : "No se han encontrado campañas que coincidan con los filtros."}</p>
-                            </Grid>
-                        }
-                    </Grid>
-
-                    <ReactPaginate
-                    breakLabel='...'
-                    nextLabel={<ArrowForward  />}
-                    previousLabel={<ArrowBack  />}
-                    onPageChange={handlePageClick}
-                    pageRangeDisplayed={2}
-                    pageCount={props.pageCount}
-                    renderOnZeroPageCount={null}
-                    pageClassName="page-item"
-                    pageLinkClassName="page-link"
-                    previousClassName="page-item"
-                    previousLinkClassName="page-link"
-                    nextClassName="page-item"
-                    nextLinkClassName="page-link"
-                    breakClassName="page-item"
-                    breakLinkClassName="page-link"
-                    marginPagesDisplayed={1}
-                    containerClassName="pagination"
-                    activeClassName="active"
-                    forcePage={props.page-1}
-                />
+                <Grid className="cam-container" container spacing={0} style={{alignContent:'center', alignItems:'flex-start'}}>
+                    {props.campaigns.length > 0 ?                        
+                        props.campaigns.map((cam, i) => {
+                            return (
+                                <Grid className='cam-card' item xs={12} sm={3} key={i}>
+                                    <CampaignCard
+                                        campaign={cam}
+                                        language={EN ? "en" : "es"}
+                                        amount={cam.raised + cam?.zelleRaised}
+                                    />
+                                </Grid>
+                                )
+                        })
+                        :
+                        <Grid className='no-cards' item xs={12} sm={12}>
+                            <p>{EN ? "No campaigns found that match the filters." : "No se han encontrado campañas que coincidan con los filtros."}</p>
+                        </Grid>
+                    }
+                </Grid>
             </div>
+            }
+            <ReactPaginate
+                breakLabel='...'
+                nextLabel={<ArrowForward  />}
+                previousLabel={<ArrowBack  />}
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={2}
+                pageCount={props.pageCount}
+                renderOnZeroPageCount={null}
+                pageClassName="page-item"
+                pageLinkClassName="page-link"
+                previousClassName="page-item"
+                previousLinkClassName="page-link"
+                nextClassName="page-item"
+                nextLinkClassName="page-link"
+                breakClassName="page-item"
+                breakLinkClassName="page-link"
+                marginPagesDisplayed={1}
+                containerClassName="pagination"
+                activeClassName="active"
+                forcePage={props.page-1}
+            />
+        </div>
+            
         );
     }
-}
 
 export default Pageination;
