1a242ef9375705001f9a89b33268fc3a13e2015a
remakh <reean.m@gmail.com>
2022-06-02 21:36:04 +0100

    Reverts changes

--- src/components/Pages/Donate/donate.js
+++ src/components/Pages/Donate/donate.js
@@ -12,6 +12,19 @@ import api from '../../../services/api';
 import LanguageService from '../../../services/language';
 
 
+// no way did i write this, so here is an explanation
+// https://stackoverflow.com/questions/1584370/how-to-merge-two-arrays-in-javascript-and-de-duplicate-items
+const arrayUnique = (array) => {
+    var a = array.concat();
+    for (var i=0; i<a.length; i++) {
+        for (var j=i+1; j<a.length; ++j) {
+            if (a[i] === a[j])
+                a.splice(j--, 1);
+        }
+    }
+    return a;
+}
+
 const filterCampaignsBySearch = (campaigns, query, lang) => {
     if(!query) {
       return campaigns;
@@ -28,7 +41,7 @@ const filterCampaignsBySearch = (campaigns, query, lang) => {
         return campaignDescription.includes(query.toLowerCase());
     });
 
-    return Set(searchTitles.concat(searchDescriptions));
+    return arrayUnique(searchTitles.concat(searchDescriptions));
 };
 
 const filterCampaignsByCategory = (campaigns, categoryState) => {
