55d4b692ff619d9144d03c8b48730f35f05f8e62
DaHunterTime <ignacio.huntley@uc.cl>
2022-07-04 13:57:07 -0400

    Implemented suggested changes

--- src/components/author.js
+++ src/components/author.js
@@ -5,8 +5,18 @@ import LanguageService from "../services/language";
 
 import './author.css';
 
+function SocialMediaIcon(props) {
+    return (
+        <div className={`m${props.iconMargin}-1`}>
+            <a id="icon" href={props.iconLink} rel="noopener noreferrer" target="_blank">
+                <i className={`fab ${props.iconClass} fa-3x footer-icon`} ></i>
+            </a>
+        </div>
+    );
+}
+
 function Author() {
-    const [language, setLanguage] = useState('');
+    const [EN, setEN] = useState(LanguageService.getLanguage() === 'en');
     const [width, setWidth] = useState(window.innerWidth);
 
     const handleWindowSize = () => {
@@ -16,7 +26,7 @@ function Author() {
     let isMobile = width < 700 ? true : false;
 
     useEffect(() => {
-        setLanguage(LanguageService.getLanguage());
+        setEN(LanguageService.getLanguage() === 'en');
 
         window.addEventListener('resize', handleWindowSize);
         return () => window.removeEventListener('resize', handleWindowSize);
@@ -40,14 +50,32 @@ function Author() {
         window.location.href = 'mailto: raul@yakera.org'
     }
 
-    var EN;
-    if (language === "en") {
-        EN = true
-    } else {
-        EN = false
-    }
+    let copyrightText = `© ${new Date().getFullYear()} Yakera`;
 
-    let bottomSectionClass = isMobile ? 'section section-mobile' : 'section';
+    let socialMedia = [
+        {iconLink: 'https://www.facebook.com/yakeraorg', iconClass: 'fa-facebook-square'},
+        {iconLink: 'https://twitter.com/yakeraorg', iconClass: 'fa-twitter-square'},
+        {iconLink: 'https://www.instagram.com/yakeraorg', iconClass: 'fa-instagram'},
+        // {iconLink: 'https://medium.com/@yakera.venezuela/yakera-re-imagining-peer-to-peer-aid-for-venezuelans-793024ac9767', iconClass: 'fa-medium'},
+    ];
+
+    let socialMediaIcons = [];
+
+    socialMedia.forEach((value, index) => {
+        let iconMargin;
+
+        if (index === 0) {
+            iconMargin = 'r';
+        }
+        else if (index === socialMedia.length - 1) {
+            iconMargin = 'l';
+        }
+        else {
+            iconMargin = 'x';
+        }
+
+        socialMediaIcons.push(<SocialMediaIcon iconMargin={iconMargin} iconLink={value.iconLink} iconClass={value.iconClass} />)
+    });
 
     return (
         <div className='footer'>
@@ -55,47 +83,24 @@ function Author() {
             <div className='section'>
                 <div className="col">
                     <div className="row justify-content-between">
+                        <img onClick={onImgClick} src={logo} alt='yakera-logo' className={`yakera-footer-logo${isMobile ? '-mobile' : ''} left-margin`} />
                         {isMobile
                         ?
-                        <img onClick={onImgClick} src={logo} alt='yakera-logo' className="yakera-footer-logo-mobile left-margin" />
+                        ''
                         :
-                        <>
-                        <img onClick={onImgClick} src={logo} alt='yakera-logo' className="yakera-footer-logo left-margin" />
                         <div className="row">
-                            <div className="mr-2">
+                            <div className="mr-2 vertical-align">
                                 <button id='link' name='contact' onClick={EN ? sendMail : contactUs}>{EN ? 'Contact Us' : 'Contáctanos'}</button>
                             </div>
-                            <div className="mx-2">
+                            <div className="mx-2 vertical-align">
                                 <button id='link' name='about' onClick={onLinkClick}>{EN ? 'About Us' : 'Sobre nosotros'}</button>
                             </div>
-                            <div className="ml-2">
+                            <div className="ml-2 vertical-align">
                                 <button id='link' name='terms' onClick={onLinkClick}>{EN ? 'Terms & Conditions' : 'Términos y condiciones'}</button>
                             </div>
-                        </div>
-                        </>}
+                        </div>}
                         <div className="row right-margin">
-                            <div className="mr-1">
-                                <a id="icon" href={'https://www.facebook.com/yakeraorg'} rel="noopener noreferrer" target="_blank">
-                                    <i className="fab fa-facebook-square fa-3x icon-color" ></i>
-                                </a>
-                            </div>
-                            <div className="mx-1">
-                                <a id="icon" href={"https://twitter.com/yakeraorg"} rel="noopener noreferrer" target="_blank">
-                                    <i className="fab fa-twitter-square fa-3x icon-color" ></i>
-                                </a>
-                            </div>
-                            <div className="ml-1">
-                                <a id="icon" href={"https://www.instagram.com/yakeraorg"} rel="noopener noreferrer" target="_blank">
-                                    <i className="fab fa-instagram fa-3x icon-color" ></i>
-                                </a>
-                            </div>
-                            {/*
-                            <div className="ml-1">
-                                <a id="icon" href={"https://medium.com/@yakera.venezuela/yakera-re-imagining-peer-to-peer-aid-for-venezuelans-793024ac9767"} rel="noopener noreferrer" target="_blank">
-                                    <i className="fab fa-medium fa-3x icon-color" ></i>
-                                </a>
-                            </div>
-                            */}
+                            {socialMediaIcons}
                         </div>
                     </div>
                 </div>
@@ -103,7 +108,7 @@ function Author() {
 
             {isMobile ? '' : <hr />}
 
-            <div className={bottomSectionClass}>
+            <div className={`section${isMobile ? ' section-mobile' : ''}`}>
                 {/*EN ? <p>This page is developed by the <b>Software Engineering Team</b> at <b>Yakera</b> and belongs to the Yakera company.</p> : <p>Esta página está desarrollada por el <b> Equipo de Ingeniería de Software </b> en <b> Yakera </b> y pertenece a la empresa Yakera.</p>*/}
                 {isMobile
                 ?
@@ -116,17 +121,17 @@ function Author() {
                     </div>
                     <div className="row justify-content-between">
                         <button className="mobile-btn" id='link' name='terms' onClick={onLinkClick}>{EN ? 'Terms & Conditions' : 'Términos y condiciones'}</button>
-                        <p className="footer-text copyright">© 2022 Yakera</p>
+                        <p className="footer-text copyright">{copyrightText}</p>
                     </div>
                 </div>
                 :
                 <div className="row">
                     <div className="col"></div>
-                    <div className="col-6">
+                    <div className="col-6 datacamp">
                         {EN ? <p className="footer-text">Partnering with <img className='footer-img' alt='datacamp-logo' src={datacamplogo} onClick={() => window.location.href = "https://www.datacamp.com/donates"} /> for data education.</p> : <p>Asociación con <img className='footer-img' alt='datacamp-logo' src={datacamplogo} /> para la educación de datos.</p>}
                     </div>
                     <div className="col right-margin">
-                        <p className="footer-text copyright">© 2022 Yakera</p>
+                        <p className="footer-text copyright">{copyrightText}</p>
                     </div>
                 </div>
                 }
