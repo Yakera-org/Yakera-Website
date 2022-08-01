9d3c24d909f48a17fab03aa1ca1fcba4527f8658
jango707 <jangbel99@gmail.com>
2022-08-01 20:01:14 +0100

    Format FAQ pages

--- src/components/Pages/FAQ/FAQDrawer.js
+++ src/components/Pages/FAQ/FAQDrawer.js
@@ -1,55 +1,40 @@
-import React from 'react'
-import { Grid } from '@material-ui/core';
-import FAQQuestion from './FAQQuestion';
+import React from "react";
+import { Grid } from "@material-ui/core";
+import FAQQuestion from "./FAQQuestion";
 
 function FAQDrawer(props) {
-      const title = props.title
-      const questions = props.questions
-      const answers = props.answers
-      const image = props.image
+  const title = props.title;
+  const questions = props.questions;
+  const answers = props.answers;
+  const image = props.image;
 
-      const aboutYakera = "https://yakera-files.s3.us-east-2.amazonaws.com/yakera/aboutYakera.webp";
-      const createCampaign = "https://yakera-files.s3.us-east-2.amazonaws.com/yakera/createCampaign.webp";
-      const withdrawing = "https://yakera-files.s3.us-east-2.amazonaws.com/yakera/withdrawing.webp";
-      const supportUs = "https://yakera-files.s3.us-east-2.amazonaws.com/yakera/supportUs.webp";
+  const yakeraFilesBase = "https://assets.yakera.org/yakera/";
 
-      const nameToImage = {
-        "aboutYakera": aboutYakera,
-        "createCampaign": createCampaign,
-        "withdrawing": withdrawing,
-        "supportUs": supportUs,
-      }
-
-    return (
-        <div>
-            <Grid container spacing={1} style={{ alignItems:'center'}}>
-                <Grid item xs={12} sm={12} >
-                    <div className='banner-img-mweb'><img alt='banner-img' className="banner-img" src={nameToImage[image]}/></div>
-                    <div className='drawer-title'>{title}</div>
-                </Grid>
-                <div className='faq-question-area'>
-                    {
-                        questions.map((question, i) => {
-                            return(
-                                <div key={i}>
-                                    <FAQQuestion
-                                        question={question}
-                                        answer={answers[i]}
-                                        
-                                    />
-                                   
-                                </div>
-                                )
-                        })
-
-
-
-                    }
-                </div>
-            </Grid>
-                  
-      </div>
-    )
+  return (
+    <div>
+      <Grid container spacing={1} style={{ alignItems: "center" }}>
+        <Grid item xs={12} sm={12}>
+          <div className="banner-img-mweb">
+            <img
+              alt="banner-img"
+              className="banner-img"
+              src={yakeraFilesBase + image + ".webp"}
+            />
+          </div>
+          <div className="drawer-title">{title}</div>
+        </Grid>
+        <div className="faq-question-area">
+          {questions.map((question, i) => {
+            return (
+              <div key={i}>
+                <FAQQuestion question={question} answer={answers[i]} />
+              </div>
+            );
+          })}
+        </div>
+      </Grid>
+    </div>
+  );
 }
 
-export default FAQDrawer
+export default FAQDrawer;
