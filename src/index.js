cb9b36938d6ffe657c2f98c731170fe2eedef31e
jango707 <jangbel99@gmail.com>
2022-08-04 11:14:05 +0100

    Hide sentry in local

--- src/index.js
+++ src/index.js
@@ -6,16 +6,19 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
+import Environment from "./services/Environment";
 
-Sentry.init({
-  dsn: "https://f66de6b062154ac489849554d9cdd641@o1334116.ingest.sentry.io/6600283",
-  integrations: [new BrowserTracing()],
+if (Environment.getCurrentEnvironment() === "production") {
+  Sentry.init({
+    dsn: "https://f66de6b062154ac489849554d9cdd641@o1334116.ingest.sentry.io/6600283",
+    integrations: [new BrowserTracing()],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
 
 ReactDOM.render(
   <React.StrictMode>
--- src/services/Environment.js
+++ src/services/Environment.js
@@ -1,29 +1,36 @@
 const ENVIRONMENTS = {
-    production: "production",
-    dev: "dev"
-}
+  production: "production",
+  dev: "dev",
+};
 
 //change this next line
-const currentEnv = ENVIRONMENTS.dev
+const currentEnv = ENVIRONMENTS.dev;
 
 const getBackendUrl = () => {
-    let backendUrl = "https://api.yakera.org/api"
-    if(currentEnv===ENVIRONMENTS.dev){
-        backendUrl = "https://yakera-backend.herokuapp.com/api"
-    }
-    return backendUrl
-}
+  let backendUrl = "https://api.yakera.org/api";
+  if (currentEnv === ENVIRONMENTS.dev) {
+    backendUrl = "https://yakera-backend.herokuapp.com/api";
+  }
+  return backendUrl;
+};
 const getStripeToken = () => {
-    let stripeKey = "pk_live_51KjTNTD1ctBA5rzvx5iDGT7idBMfOQjoZ8Ic3MbbQTPg14bMM9aIURbyUTURypVEkqvsJK5jNBUD4DocFRSuK79B00cuzpMyr8"
-    if(currentEnv===ENVIRONMENTS.dev){
-        stripeKey = "pk_test_51KjTNTD1ctBA5rzvPq6FjtoOxn2bGAPvUX5GluRXOUnaMrINHjQ55uC3ZqllRDaUcoTAITPjPlvT76cNjNlZAPTM00Y71uOjrE"
-    }
-    return stripeKey
-}
+  let stripeKey =
+    "pk_live_51KjTNTD1ctBA5rzvx5iDGT7idBMfOQjoZ8Ic3MbbQTPg14bMM9aIURbyUTURypVEkqvsJK5jNBUD4DocFRSuK79B00cuzpMyr8";
+  if (currentEnv === ENVIRONMENTS.dev) {
+    stripeKey =
+      "pk_test_51KjTNTD1ctBA5rzvPq6FjtoOxn2bGAPvUX5GluRXOUnaMrINHjQ55uC3ZqllRDaUcoTAITPjPlvT76cNjNlZAPTM00Y71uOjrE";
+  }
+  return stripeKey;
+};
+
+const getCurrentEnvironment = () => {
+  return currentEnv;
+};
 
 const Environment = {
-    getStripeToken,
-    getBackendUrl
+  getStripeToken,
+  getBackendUrl,
+  getCurrentEnvironment,
 };
-  
-  export default Environment;
+
+export default Environment;
