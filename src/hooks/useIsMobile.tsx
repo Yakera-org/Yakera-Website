1560818c885bed16b8a2cb8c9888d474a853c6fe
jango707 <jangbel99@gmail.com>
2022-09-04 12:27:09 +0100

    responsive mobile design

--- src/hooks/useIsMobile.tsx
+++ src/hooks/useIsMobile.tsx
@@ -3,7 +3,11 @@ import { useEffect, useState } from "react";
 const useIsMobile = (): boolean => {
   const [mobile, setMobile] = useState(true);
   useEffect(() => {
-    setMobile(window.innerWidth < 800);
+    const handleResize = () => {
+      setMobile(window.innerWidth < 800);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
   }, []);
 
   return mobile;
